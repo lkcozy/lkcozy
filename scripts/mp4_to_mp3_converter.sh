ba71c6a652d130b1530a2023486957de52c493ba
lkcozy <lkklcozy@gmail.com>
2022-03-28 17:32:09 -0600

    feat: add mp4 to mp3 converter script

---
+++ scripts/mp4_to_mp3_converter.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# TODO: enable multiprocessing
+# TODO: convert to zx
+# TODO: Converting mp4 to mp3 after adding a new file with macos folder action
+
+# From https://askubuntu.com/questions/837916/convert-mp4-to-mp3-using-shell-script
+
+videos=$(find . -name '*.mp4')
+sounds=()
+sampleRate="48000"
+for video in $videos; do
+    sound=${video%.mp4}.mp3
+    if [ ! -f "$sound" ]; then
+        ffmpeg -i "$video" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar "$sampleRate" "$sound"
+        sounds+=("$sound")
+    fi
+done
+
+echo "Converted ${#sounds[@]} vidoes"
+
+for i in "${sounds[*]}"; do
+    echo -e "$i\n"
+done
+
+# find . -name "*.mp4" -exec bash -c 'ffmpeg -i "$1" -vn -acodec libmp3lame -ac 2 -qscale:a 4 -ar 48000 "${1%.mp4}".mp3' - '{}' \;
+
+# echo "Cleaning up mp3 files"
+# find . -name "*.mp3" -exec bash -c 'var=${1%.mp3}; var=${var#*/}; rm "${1#*/}"' - '{}' \;
+
+# Explanation
+# find . -name "*.mp4" finds all files of .mp4 type. -exec bash -c runs the bash command provided in the succeeding string for each file found. It also passes the filename as variable $1 which can be used in the bash command.
+
+# At this point $1 contains something like ./path/file.mp4. To remove the filetype *.mp4 we use var=${1%.mp4} which stores ./path/file to $var. Now we remove the preceding ./ with var=${var#*/} hence the value of $var is path/file.
+
+# In the ffmpeg command we get the current file name with ${var#*/} which gives path/file.mp4 and we get the output file with ~/PATH/to/NewTopDIr/$var".mp3 where $var has the value path/file as explained above.
