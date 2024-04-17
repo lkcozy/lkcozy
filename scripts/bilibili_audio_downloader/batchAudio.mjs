de64ee00e33f20a9dd0cbd83afda913567761061
lkcozy <lkklcozy@gmail.com>
2024-04-17 17:43:09 -0600

    feat: use node 21 and load urls from .env

--- scripts/bilibili_audio_downloader/batchAudio.mjs
+++ scripts/bilibili_audio_downloader/batchAudio.mjs
@@ -1,19 +1,21 @@
 #!/usr/bin/env zx
 
+process.loadEnvFile(".env"); // require node 21+
+
 // Define an array of Bilibili video URLs
-const urls = []; // ["url1","url2"]
+const urls = ($.env.URLS || "").split(",");
 
 const options = [
   "--audio-only",
   "--skip-cover",
   "--skip-subtitle",
-  "--hide-streams",
-  "--work-dir",
+  "--hide-streams", // 不要显示所有可用音视频流
+  "--work-dir", // 下载文件存放目录
   "download",
 ];
 
 async function downloadLatestBBDown() {
-  const currentVersion = "1.6.1";
+  const currentVersion = "1.6.0";
   const targetOperateSystem = "osx-x64";
   const releaseUrl =
     "https://api.github.com/repos/nilaoda/BBDown/releases/latest";
@@ -28,10 +30,12 @@ async function downloadLatestBBDown() {
   );
 
   const { stdout } =
-    await $`curl -s ${releaseUrl} | jq '.assets[] | select(.name | contains(${targetOperateSystem})) | .browser_download_url'`;
+    await $`curl -s ${releaseUrl} | jq '.assets[] | select(.name | contains("${targetOperateSystem}")) | .browser_download_url'`;
   const url = stdout.trim().replace(/"/g, "");
   console.log("BBDown latest url:", chalk.bgGreen.bold(url));
-  $`curl -Lk ${url} -o temp.zip && unzip -o temp.zip && chmod +x BBDown && rm temp.zip`;
+  const tempFileName = `temp.zip`;
+  await $`curl -Lk ${url} -o ${tempFileName} && unzip -o ${tempFileName} && chmod +x BBDown && rm ${tempFileName}`;
+  console.log("Download finished. ", chalk.bgGreen.bold(latestVersion));
 }
 
 async function downloadAudios() {
@@ -42,4 +46,5 @@ async function downloadAudios() {
 }
 
 await downloadLatestBBDown();
+
 await downloadAudios();
